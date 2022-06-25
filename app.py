345d4cd18e7d94c9a046f18fba52b033e1080ddf
Melli <niamelli11@gmail.com>
2022-06-25 19:16:48 +0800

    initial

--- app.py
+++ app.py
@@ -4,17 +4,17 @@ import requests
 #Untuk menjelaskan nama modul yang digunakan, sehingga ketika folder lain memanggil folder app akan otomatis teridentifikasi.
 app = Flask(__name__) 
 
-@app.route('/', methods=['GET', 'POST'])
-def parse_request():
-    data = request.data
-    if request.method == 'GET':
-        return 'Make get' + data
-    else:
-        return 'Make post' + data
+# @app.route('/', methods=['GET', 'POST'])
+# def parse_request():
+#     data = request.data
+#     if request.method == 'GET':
+#         return 'Make get' + data
+#     else:
+#         return 'Make post' + data
 
     # use data here
 
 
-# @app.route("/")
-# def home():
-#     return "Hello, Flasks!"
+@app.route("/")
+def home():
+    return "Hello, Flasks!"
