a5a2db7672fc93dfe25c067106db6d859c838368
Melli <niamelli11@gmail.com>
2022-06-25 18:28:30 +0800

    initial commit

---
+++ app.py
@@ -0,0 +1,20 @@
+from flask import Flask
+import request
+
+#Untuk menjelaskan nama modul yang digunakan, sehingga ketika folder lain memanggil folder app akan otomatis teridentifikasi.
+app = Flask(__name__) 
+
+@app.route('/', methods=['GET', 'POST'])
+def parse_request():
+    data = request.data
+    if request.method == 'GET':
+        return 'Make get' + data
+    else:
+        return 'Make post' + data
+
+    # use data here
+
+
+# @app.route("/")
+# def home():
+#     return "Hello, Flasks!"
